afc6f34c9f51d06002d3a4aa4fdb69cfe1fd1703
Ali Raza <alirazahaider2@gmail.com>
2026-05-16 23:45:03 +0500

    feat(user auth): add email account activation flow
    add reusable nodemailer utility for sending emails
    update user registration route to send activation email instead of immediate user creation
    add activation endpoint to verify JWT token and save new user to database
    add helper function to generate 10-minute JWT activation tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,8 @@ import express from 'express';
 import User from '../models/userModel.js';
 import generateToken from '../utils/generateToken.js';
 import asyncHandler from 'express-async-handler';
+import sendMail from '../utils/sendMail.js';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -17,7 +19,7 @@ router.post(
       throw new Error('User Already Exists!');
     }
 
-    const user = await User.create({
+    const user = {
       name,
       email,
       password,
@@ -25,18 +27,51 @@ router.post(
         public_id: 'default_avatar_id',
         url: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
       },
+    };
+    const activation_token = createActivationToken(user);
+    const activationUrl = `http://localhost:8000/activation/${activation_token}`;
+
+    await sendMail({
+      email: user.email,
+      subject: 'Activate your account',
+      message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
     });
 
-    if (user) {
-      generateToken(res, user._id);
-      res.status(201).json({
-        success: true,
-        user,
-      });
-    } else {
+    res.status(201).json({
+      success: true,
+      message: `please check your email:- ${user.email} to activate your account!`,
+    });
+  })
+);
+
+// activation of account and user creation
+router.post(
+  '/activation/:activation_token',
+  asyncHandler(async (req, res, next) => {
+    const { activation_token } = req.params;
+
+    const newUser = jwt.verify(activation_token, process.env.JWT_SECRET);
+
+    if (!newUser) {
       res.status(400);
-      throw new Error('Invalid user data');
+      throw new Error('Invalid token');
     }
+
+    const { name, email, password, avatar } = newUser;
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      avatar,
+    });
+
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      user,
+    });
   })
 );
 
@@ -49,18 +84,17 @@ router.post(
       res.status(400);
       throw new Error('Please provide email and password');
     }
-
+    //force select password to compare the hashed and entered password
     const user = await User.findOne({ email }).select('+password');
 
     if (user && (await user.comparePassword(password))) {
       generateToken(res, user._id);
-
+      //set password to undefined to clear hashed password
       user.password = undefined;
 
       res.status(200).json({
         success: true,
         message: 'Login Successful',
-        user,
       });
     } else {
       res.status(401);
@@ -68,5 +102,9 @@ router.post(
     }
   })
 );
-
+const createActivationToken = (user) => {
+  return jwt.sign(user, process.env.JWT_SECRET, {
+    expiresIn: '10m',
+  });
+};
 export default router;
