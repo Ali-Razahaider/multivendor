1f46bbd3e74158180dba56b8d93ab845bcd9b3c1
Ali Raza <alirazahaider2@gmail.com>
2026-06-29 13:03:20 +0500

    update ids

--- frontend/src/static/data.jsx
+++ frontend/src/static/data.jsx
@@ -103,7 +103,7 @@ export const brandingData = [
     ),
   },
   {
-    id: 4,
+    id: 3,
     title: "Affortable Prices",
     Description: "Get Factory direct price",
     icon: (
@@ -150,7 +150,7 @@ export const brandingData = [
     ),
   },
   {
-    id: 5,
+    id: 4,
     title: "Secure Payments",
     Description: "100% protected payments",
     icon: (
@@ -190,69 +190,69 @@ export const brandingData = [
 // categories data
 export const categoriesData = [
   {
-    id: 1,
+    id: 5,
     title: "Computers and Laptops",
     subTitle: "",
     image_Url:
       "https://cdn.shopify.com/s/files/1/1706/9177/products/NEWAppleMacbookProwithM1ProChip14InchLaptop2021ModelMKGQ3LL_A_16GB_1TBSSD_custommacbd.jpg?v=1659592838",
   },
   {
-    id: 2,
+    id: 6,
     title: "cosmetics and body care",
     subTitle: "",
     image_Url:
       "https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2021-07/kosme1.png",
   },
   {
-    id: 3,
+    id: 7,
     title: "Accesories",
     subTitle: "",
     image_Url:
       "https://img.freepik.com/free-vector/ordering-goods-online-internet-store-online-shopping-niche-e-commerce-website-mother-buying-babies-clothes-footwear-toys-infant-accessories_335657-2345.jpg?w=2000",
   },
   {
-    id: 4,
+    id: 8,
     title: "Cloths",
     subTitle: "",
     image_Url:
       "https://www.shift4shop.com/2015/images/industries/clothing/clothing-apparel.png",
   },
   {
-    id: 5,
+    id: 9,
     title: "Shoes",
     subTitle: "",
     image_Url:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvBQPQMVNRd6TtDkGs2dCri0Y-rxKkFOiEWw&usqp=CAU",
   },
   {
-    id: 6,
+    id: 10,
     title: "Gifts",
     subTitle: "",
     image_Url:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvBQPQMVNRd6TtDkGs2dCri0Y-rxKkFOiEWw&usqp=CAU",
   },
   {
-    id: 7,
+    id: 11,
     title: "Pet Care",
     subTitle: "",
     image_Url: "https://cdn.openpr.com/T/c/Tc15444071_g.jpg",
   },
   {
-    id: 8,
+    id: 12,
     title: "Mobile and Tablets",
     subTitle: "",
     image_Url:
       "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mpwp3tua-apple-iphone-14-256gb-mavi-mpwp3tua-637986832343472449.jpg",
   },
   {
-    id: 9,
+    id: 13,
     title: "Music and Gaming",
     subTitle: "",
     image_Url:
       "https://static.vecteezy.com/system/resources/previews/011/996/555/original/3d-black-headphone-illustration-ecommerce-icon-png.png",
   },
   {
-    id: 10,
+    id: 14,
     title: "Others",
     subTitle: "",
     image_Url:
@@ -263,7 +263,7 @@ export const categoriesData = [
 // product Data
 export const productData = [
   {
-    id: 1,
+    id: 15,
     category: "Computers and Laptops",
     name: "MacBook pro M2 chipset 256gb ssd 8gb ram space-gray color with apple 1 year warranty",
     description:
@@ -293,7 +293,7 @@ export const productData = [
     stock: 10,
   },
   {
-    id: 2,
+    id: 16,
     category: "Mobile and Tablets",
     name: "Iphone 14 pro max 256 gb ssd and 8 gb ram silver colour",
     description:
@@ -323,7 +323,7 @@ export const productData = [
     category: "Mobile & Tablets",
   },
   {
-    id: 1,
+    id: 17,
     category: "Computers and Laptop",
     name: "MacBook pro M2 chipset 256gb ssd 8gb ram space gray color with apple 1 year warranty",
     description:
@@ -354,7 +354,7 @@ export const productData = [
     category: "Computers & Laptop",
   },
   {
-    id: 4,
+    id: 18,
     category: "Others",
     name: "New Fashionable Watch for men 2023 with multiple colors",
     description:
@@ -385,7 +385,7 @@ export const productData = [
     stock: 10,
   },
   {
-    id: 5,
+    id: 19,
     category: "Shoes",
     name: "New Trend shoes for gents with all sizes",
     description:
@@ -416,7 +416,7 @@ export const productData = [
     category: "Shoes",
   },
   {
-    id: 1,
+    id: 20,
     name: "Gaming Headphone Asus with mutiple color and free delivery",
     description:
       "Product details are a crucial part of any eCommerce website or online marketplace. These details help the potential customers to make an informed decision about the product they are interested in buying. A well-written product description can also be a powerful marketing tool that can help to increase sales.Product details typically include information about the product's features, specifications, dimensions, weight, materials, and other relevant information that can help customers to understand the product better. The product details section should also include high-quality images and videos of the product, as well as customer reviews and ratings.",
@@ -455,7 +455,7 @@ export const productData = [
     category: "Music and Gaming",
   },
   {
-    id: 4,
+    id: 21,
     name: "New Fashionable Watch for men 2023 with multiple colors",
     description:
       "Product details are a crucial part of any eCommerce website or online marketplace. These details help the potential customers to make an informed decision about the product they are interested in buying. A well-written product description can also be a powerful marketing tool that can help to increase sales.Product details typically include information about the product's features, specifications, dimensions, weight, materials, and other relevant information that can help customers to understand the product better. The product details section should also include high-quality images and videos of the product, as well as customer reviews and ratings.",
@@ -484,7 +484,7 @@ export const productData = [
     stock: 10,
   },
   {
-    id: 1,
+    id: 22,
     name: "Gaming Headphone Asus with mutiple color and free delivery",
     description:
       "Product details are a crucial part of any eCommerce website or online marketplace. These details help the potential customers to make an informed decision about the product they are interested in buying. A well-written product description can also be a powerful marketing tool that can help to increase sales.Product details typically include information about the product's features, specifications, dimensions, weight, materials, and other relevant information that can help customers to understand the product better. The product details section should also include high-quality images and videos of the product, as well as customer reviews and ratings.",
@@ -522,7 +522,7 @@ export const productData = [
     stock: 10,
   },
   {
-    id: 2,
+    id: 23,
     category: "Mobile and Tablets",
     name: "Iphone 14 pro max 256 gb ssd and 8 gb ram silver colour",
     description:
@@ -551,7 +551,7 @@ export const productData = [
     stock: 10,
   },
   {
-    id: 1,
+    id: 24,
     category: "Music and Gaming",
     name: "Gaming Headphone Asus with mutiple color and free delivery",
     description:
